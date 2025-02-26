a57912144e4144756d8043049cb9339e738623c6
angelinabrgh <angelina.b4@outlook.de>
2025-02-26 15:40:53 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import SustainabilitySurvey from "../SustainabilitySurvey";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <SustainabilitySurvey />
+    </div>
+  );
+}
