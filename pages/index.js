31a524b5e1acafa59b6f9dc0fd0af98b8aa3f87d
angelinabrgh <angelina.b4@outlook.de>
2025-03-10 19:11:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import SustainabilitySurvey from "../SustainabilitySurvey";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-100 to-green-300">
       <SustainabilitySurvey />
     </div>
   );
