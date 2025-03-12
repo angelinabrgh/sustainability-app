10184b683db56245eb1cb99a9708c99a5843b02a
angelinabrgh <angelina.b4@outlook.de>
2025-03-12 16:31:19 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'nature': "url('https://cdn.pixabay.com/photo/2016/11/29/04/17/sunrise-1865138_1920.jpg')",
+      }
+    }
   },
   plugins: [],
 };
